373bb3135e594014be50a6fb9313891a345c778a
Boris Yangel <hr0nix@yandex-team.ru>
2021-08-17 01:19:38 +0300

    Cast densenet inputs to processing dim first.

--- omega/neural/dense.py
+++ omega/neural/dense.py
@@ -8,6 +8,10 @@ class DenseNet(nn.Module):
     activation: str = 'relu'
 
     def setup(self):
+        self._input_dense = nn.Dense(
+            self.dim,
+            name='{}/input_dense'.format(self.name)
+        )
         self._dense = [
             nn.Dense(
                 self.dim,
@@ -25,9 +29,6 @@ class DenseNet(nn.Module):
             )
             for block_idx in range(self.num_blocks)
         ]
-        self._final_norm = nn.LayerNorm(
-            name='{}/final_norm'.format(self.name)
-        )
 
     def _activation(self, input):
         if self.activation == 'relu':
@@ -36,12 +37,11 @@ class DenseNet(nn.Module):
             raise ValueError('Unknown activation: {}'.format(self.activation))
 
     def __call__(self, input):
-        t = input
+        t = self._input_dense(input)
         for block_idx in range(self.num_blocks):
             t_prev = t
+            t = self._norm[block_idx](t)
             t = self._dense[block_idx](t)
             t = self._activation(t)
-            t = self._norm[block_idx](t)
             t += t_prev
-        t = self._final_norm(t)
         return self._output_dense(t)
