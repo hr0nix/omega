84035ef7bef47438d1d3158af5fad911f6a0f955
Boris Yangel <hr0nix@yandex-team.ru>
2021-09-04 23:40:18 +0300

    A couple of helper scripts for running experiments.

---
+++ tools/run_experiment.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: <experiment_name>"
+    exit 1
+fi
+
+if [ -z $OMEGA_EXPERIMENTS_DIR ]; then
+  echo "OMEGA_EXPERIMENTS_DIR environment variable is not set"
+  exit 1
+fi
+
+EXPERIMENTS_DIR=$OMEGA_EXPERIMENTS_DIR
+EXP_DIR=$EXPERIMENTS_DIR/$1
+
+if [[ ! -d $EXPERIMENTS_DIR ]]; then
+  echo "Experiments dir $EXPERIMENTS_DIR does not exist"
+  exit 1
+fi
+
+if [[ ! -d $EXP_DIR ]]; then
+  echo "Experiment dir $EXP_DIR does not exist"
+  exit 1
+fi
+
+python3.8 ./tools/train_agent.py --config $EXP_DIR/config.yaml --checkpoints $EXP_DIR/checkpoints --game-logs $EXP_DIR/games --tb-logs $EXP_DIR/logs
