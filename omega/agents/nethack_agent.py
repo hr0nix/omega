616726c1533f9bac7f718e3e4e3777a2c5adfdc6
Boris Yangel <hr0nix@yandex-team.ru>
2021-10-03 18:06:55 +0300

    Perceiver pos embeddings, ability to disable the use of BL stats.

--- omega/agents/nethack_agent.py
+++ omega/agents/nethack_agent.py
@@ -27,6 +27,7 @@ class NethackPerceiverModel(nn.Module):
     glyph_embedding_dim: int = 64
     num_memory_units: int = 128
     memory_dim: int = 64
+    use_bl_stats: bool = True
     num_bl_stats_blocks: int = 2
     num_perceiver_blocks: int = 2
     num_perceiver_self_attention_subblocks: int = 2
@@ -37,12 +38,18 @@ class NethackPerceiverModel(nn.Module):
     num_policy_network_blocks: int = 4
     num_value_network_blocks: int = 4
     deterministic: Optional[bool] = None
+    use_fixed_positional_embeddings: bool = False
+    positional_embeddings_num_bands: int = 32
+    positional_embeddings_max_freq: int = 80
 
     def setup(self):
-        self._glyph_pos_embedding = nn.Embed(
-            num_embeddings=nle.nethack.DUNGEON_SHAPE[0] * nle.nethack.DUNGEON_SHAPE[1],
-            features=self.glyph_embedding_dim
-        )
+        if not self.use_fixed_positional_embeddings:
+            self._glyph_pos_embedding = nn.Embed(
+                num_embeddings=nle.nethack.DUNGEON_SHAPE[0] * nle.nethack.DUNGEON_SHAPE[1],
+                features=self.glyph_embedding_dim
+            )
+        else:
+            self._glyph_pos_embedding_processor = nn.Dense(self.memory_dim)
         self._glyph_embedding = nn.Embed(
             num_embeddings=nle.nethack.MAX_GLYPH + 1,
             features=self.glyph_embedding_dim
@@ -75,9 +82,10 @@ class NethackPerceiverModel(nn.Module):
             )
             for block_idx in range(self.num_perceiver_blocks)
         ]
-        self._bl_stats_network = DenseNet(
-            num_blocks=self.num_bl_stats_blocks, dim=self.memory_dim, output_dim=self.memory_dim,
-        )
+        if self.use_bl_stats:
+            self._bl_stats_network = DenseNet(
+                num_blocks=self.num_bl_stats_blocks, dim=self.memory_dim, output_dim=self.memory_dim,
+            )
         self._policy_network = DenseNet(
             num_blocks=self.num_policy_network_blocks, dim=self.memory_dim, output_dim=self.num_actions,
         )
@@ -85,29 +93,62 @@ class NethackPerceiverModel(nn.Module):
             num_blocks=self.num_value_network_blocks, dim=self.memory_dim, output_dim=1,
         )
 
-    def __call__(self, observations_batch, deterministic=None, rng=None):
+    def _make_fixed_pos_embeddings(self):
+        logf = jnp.linspace(
+            start=0.0,
+            stop=jnp.log(0.5 * self.positional_embeddings_max_freq),
+            num=self.positional_embeddings_num_bands,
+            dtype=jnp.float32,
+        )
+        f = jnp.exp(logf)
+
+        r_coords = jnp.linspace(-1.0, 1.0, num=nle.nethack.DUNGEON_SHAPE[0])
+        c_coords = jnp.linspace(-1.0, 1.0, num=nle.nethack.DUNGEON_SHAPE[1])
+        x_2d, y_2d = jnp.meshgrid(r_coords, c_coords, indexing='ij')
+        coords = jnp.stack([x_2d, y_2d], axis=-1)
+
+        cfp = jnp.pi * jnp.einsum('...c,f->...cf', coords, f)
+        cfp = jnp.reshape(
+            cfp,
+            (
+                nle.nethack.DUNGEON_SHAPE[0],
+                nle.nethack.DUNGEON_SHAPE[1],
+                2 * self.positional_embeddings_num_bands
+            )
+        )
+        sin_cfp = jnp.sin(cfp)
+        cos_cfp = jnp.cos(cfp)
+
+        pos_embeddings = jnp.concatenate([sin_cfp, cos_cfp, coords], axis=-1)
+        pos_embeddings = self._glyph_pos_embedding_processor(pos_embeddings)
+        pos_embeddings = jnp.reshape(pos_embeddings, (1, -1, self.memory_dim))
+
+        return pos_embeddings
+
+    def __call__(self, observations_batch, rng, deterministic=None):
         deterministic = nn.module.merge_param('deterministic', self.deterministic, deterministic)
-        if rng is None:
-            rng = self.make_rng('nethack_transformer_model')
 
         glyphs = observations_batch['glyphs']
         batch_size = glyphs.shape[0]
-
-        bl_stats = observations_batch['blstats']
-        bl_stats = self._bl_stats_network(bl_stats)
 
         memory_indices = jnp.arange(0, self.num_memory_units, dtype=jnp.int32)
         memory = self._memory_embedding(memory_indices)  # TODO: add memory recurrence
         memory = jnp.tile(memory, reps=[batch_size, 1, 1])
 
-        memory = memory + jnp.expand_dims(bl_stats, axis=1)  # Add global features to every memory cell
+        if self.use_bl_stats:
+            bl_stats = observations_batch['blstats']
+            bl_stats = self._bl_stats_network(bl_stats)
+            memory = memory + jnp.expand_dims(bl_stats, axis=1)  # Add global features to every memory cell
 
         glyphs = jnp.reshape(glyphs, newshape=(glyphs.shape[0], -1))
         glyphs_embeddings = self._glyph_embedding(glyphs)
 
-        glyph_pos_indices = jnp.arange(
-            0, nle.nethack.DUNGEON_SHAPE[0] * nle.nethack.DUNGEON_SHAPE[1], dtype=jnp.int32)
-        glyph_pos_embeddings = self._glyph_pos_embedding(glyph_pos_indices)
+        if self.use_fixed_positional_embeddings:
+            glyph_pos_embeddings = self._make_fixed_pos_embeddings()
+        else:
+            glyph_pos_indices = jnp.arange(
+                0, nle.nethack.DUNGEON_SHAPE[0] * nle.nethack.DUNGEON_SHAPE[1], dtype=jnp.int32)
+            glyph_pos_embeddings = self._glyph_pos_embedding(glyph_pos_indices)
         glyphs_embeddings += glyph_pos_embeddings
 
         for block_idx in range(self.num_perceiver_blocks):
