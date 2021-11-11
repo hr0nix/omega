1210a0cbb7376a1f0c605d7fa5ab349d3a42c4a8
Boris Yangel <hr0nix@yandex-team.ru>
2021-11-12 00:56:57 +0300

    Get rid of unnecessary stats.

--- omega/evaluation/eval_stats.py
+++ omega/evaluation/eval_stats.py
@@ -2,7 +2,7 @@ import numpy as np
 import jax.numpy as jnp
 
 from dataclasses import dataclass, field
-from collections import defaultdict, Counter
+from collections import defaultdict
 from typing import List, Optional
 
 
@@ -15,12 +15,6 @@ class RunningEpisodeStats(object):
 @dataclass
 class FinishedEpisodeStats(object):
     reward_sum: Optional[float] = None
-    min_reward: Optional[float] = None
-    max_reward: Optional[float] = None
-    reward_p50: Optional[float] = None
-    reward_p75: Optional[float] = None
-    reward_p95: Optional[float] = None
-    reward_p99: Optional[float] = None
     num_steps: Optional[int] = None
 
 
@@ -70,26 +64,15 @@ class EvaluationStats(object):
             self._total_steps += final_stats.num_steps
 
     def _finalize_episode_stats(self, stats):
-        sorted_rewards = list(stats.rewards)
-        sorted_rewards.sort()
-        action_counter = Counter(stats.actions)
-
         final_stats = FinishedEpisodeStats()
-        final_stats.num_steps = len(sorted_rewards)
-        final_stats.reward_sum = sum(sorted_rewards)
-        final_stats.min_reward = min(sorted_rewards)
-        final_stats.max_reward = max(sorted_rewards)
-        final_stats.reward_p50 = sorted_rewards[int(len(sorted_rewards) * 0.50)]
-        final_stats.reward_p75 = sorted_rewards[int(len(sorted_rewards) * 0.75)]
-        final_stats.reward_p95 = sorted_rewards[int(len(sorted_rewards) * 0.95)]
-        final_stats.reward_p99 = sorted_rewards[int(len(sorted_rewards) * 0.99)]
-        final_stats.top_5_actions = action_counter.most_common(5)
+        final_stats.num_steps = len(stats.rewards)
+        final_stats.reward_sum = sum(stats.rewards)
 
         self._finished_episode_stats.append(final_stats)
 
         return final_stats
 
-    def to_dict(self, include_non_scalar_stats=False, include_rolling_stats=False):
+    def to_dict(self, include_rolling_stats=False):
         if len(self._finished_episode_stats) == 0:
             return dict()
 
@@ -98,18 +81,8 @@ class EvaluationStats(object):
             'total_finished_episodes': len(self._finished_episode_stats),
             'last_episode_total_reward': self._finished_episode_stats[-1].reward_sum,
             'last_episode_steps': self._finished_episode_stats[-1].num_steps,
-            'last_episode_min_reward': self._finished_episode_stats[-1].min_reward,
-            'last_episode_max_reward': self._finished_episode_stats[-1].max_reward,
-            'last_episode_reward_p50': self._finished_episode_stats[-1].reward_p50,
-            'last_episode_reward_p75': self._finished_episode_stats[-1].reward_p75,
-            'last_episode_reward_p95': self._finished_episode_stats[-1].reward_p95,
-            'last_episode_reward_p99': self._finished_episode_stats[-1].reward_p99,
             'last_100_episode_avg_reward': np.mean([s.reward_sum for s in self._finished_episode_stats[-100:]])
         }
-        if include_non_scalar_stats:
-            result.update({
-                'last_episode_top_5_actions': self._finished_episode_stats[-1].top_5_actions
-            })
         if include_rolling_stats:
             result.update({
                 key: value.smoothed_value
@@ -122,7 +95,7 @@ class EvaluationStats(object):
             print('No finished episodes recorded yet.')
             return
 
-        stats = self.to_dict(include_non_scalar_stats=True, include_rolling_stats=False)
+        stats = self.to_dict(include_rolling_stats=False)
         title = title or 'Evaluation summary:'
 
         print(title, flush=True)
@@ -131,13 +104,6 @@ class EvaluationStats(object):
             ('Total finished episodes', 'total_finished_episodes'),
             ('Last episode total reward', 'last_episode_total_reward'),
             ('Last episode steps', 'last_episode_steps'),
-            ('Last episode min reward', 'last_episode_min_reward'),
-            ('Last episode max reward', 'last_episode_max_reward'),
-            ('Last episode reward 50%', 'last_episode_reward_p50'),
-            ('Last episode reward 75%', 'last_episode_reward_p75'),
-            ('Last episode reward 95%', 'last_episode_reward_p95'),
-            ('Last episode reward 99%', 'last_episode_reward_p99'),
-            ('Last episode top-5 actions', 'last_episode_top_5_actions'),
             ('Last 100 episodes avg reward', 'last_100_episode_avg_reward'),
         ]:
             print('  {}: {}'.format(key_title, stats[key]), flush=True)
