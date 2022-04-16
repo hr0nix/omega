36209bc3ddc09901cc2670b24d22ae8a654c046a
Boris Yangel <hr0nix@yandex-team.ru>
2022-04-16 16:47:39 +0300

    Update Docker image: add pytest

--- docker/run_container.sh
+++ docker/run_container.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker run --gpus all -it --env PYTHONPATH=/omega --env OMEGA_EXPERIMENTS_DIR=/omega/experiments --mount type=bind,source=$(realpath $(dirname $0))/../,target=/omega hr0nix/omega_env:1.4 /bin/bash
+docker run --gpus all -it --env PYTHONPATH=/omega --env OMEGA_EXPERIMENTS_DIR=/omega/experiments --mount type=bind,source=$(realpath $(dirname $0))/../,target=/omega hr0nix/omega_env:1.5 /bin/bash
