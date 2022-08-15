25aefdbe39fc89071029b12f8b0b7f6f3f509cb7
hr0nix <boris.jangel@gmail.com>
2022-08-16 02:04:16 +0300

    Update docker image version.

--- docker/run_container.sh
+++ docker/run_container.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker run --gpus all -it --user $(id -u):$(id -g) --env WANDB_CONFIG_DIR=/omega/.wandb/config --env PYTHONPATH=/omega --env HOME=/omega --mount type=bind,source=$(realpath $(dirname $0))/../,target=/omega hr0nix/omega_env:1.12 /bin/bash
+docker run --gpus all -it --user $(id -u):$(id -g) --env WANDB_CONFIG_DIR=/omega/.wandb/config --env PYTHONPATH=/omega --env HOME=/omega --mount type=bind,source=$(realpath $(dirname $0))/../,target=/omega hr0nix/omega_env:1.13 /bin/bash
