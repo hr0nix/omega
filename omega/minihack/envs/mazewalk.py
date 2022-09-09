bc6b45281f005fa0351baba90be8624bf04b6098
hr0nix <boris.jangel@gmail.com>
2022-09-09 15:22:22 +0300

    Add 2 more sizes of mazewalk env.

--- omega/minihack/envs/mazewalk.py
+++ omega/minihack/envs/mazewalk.py
@@ -8,10 +8,22 @@ class MiniHackMazeWalk11x11(MiniHackMazeWalk):
         super().__init__(*args, w=11, h=11, premapped=False, **kwargs)
 
 
+class MiniHackMazeWalk12x12(MiniHackMazeWalk):
+    def __init__(self, *args, **kwargs):
+        kwargs["max_episode_steps"] = kwargs.pop("max_episode_steps", 200)
+        super().__init__(*args, w=12, h=12, premapped=False, **kwargs)
+
+
 class MiniHackMazeWalk13x13(MiniHackMazeWalk):
     def __init__(self, *args, **kwargs):
         kwargs["max_episode_steps"] = kwargs.pop("max_episode_steps", 200)
         super().__init__(*args, w=13, h=13, premapped=False, **kwargs)
+
+
+class MiniHackMazeWalk14x14(MiniHackMazeWalk):
+    def __init__(self, *args, **kwargs):
+        kwargs["max_episode_steps"] = kwargs.pop("max_episode_steps", 200)
+        super().__init__(*args, w=14, h=14, premapped=False, **kwargs)
 
 
 register(
@@ -19,7 +31,20 @@ register(
     entry_point="omega.minihack.envs.mazewalk:MiniHackMazeWalk11x11",
 )
 
+
+register(
+    id="MiniHack-MazeWalk-12x12-v0",
+    entry_point="omega.minihack.envs.mazewalk:MiniHackMazeWalk11x11",
+)
+
+
 register(
     id="MiniHack-MazeWalk-13x13-v0",
     entry_point="omega.minihack.envs.mazewalk:MiniHackMazeWalk13x13",
+)
+
+
+register(
+    id="MiniHack-MazeWalk-14x14-v0",
+    entry_point="omega.minihack.envs.mazewalk:MiniHackMazeWalk14x14",
 )
